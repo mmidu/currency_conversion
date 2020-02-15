da9e59bb878418553cecada595ee5bb6a313237c
Marco <marco.miduri@gmail.com>
2020-02-15 21:27:39 +0100

    add convert view

--- src/webapi/convert/models.py
+++ src/webapi/convert/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-class Conversion(models.Model):
-	amount = models.FloatField()
-	src​_currency = models.CharField(max_length=3)
-	dest_currency = models.CharField(max_length=3)
-	reference_date = models.DateField(input_formats='%Y-%m-%d')
--- src/webapi/convert/views.py
+++ src/webapi/convert/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
 import os
-import xml.dom.minidom
+import xml.etree.ElementTree as ET
 
 def convert(request, amount, src_currency, dest_currency, reference_date):
 	'''
@@ -11,15 +11,36 @@ def convert(request, amount, src_currency, dest_currency, reference_date):
 
 	'''
 	file_path = os.path.join(settings.DATA_DIR, 'rates.xml')
-	doc = xml.dom.minidom.parse(file_path);
+	
+	tree = ET.parse(file_path)
+
+	root = tree.getroot()
+
+	src_rate = 0
+	dest_rate = 0
+	rate = 0
+	result = 0
+
+	for date in root[2]:
+		if date.attrib['time'] == reference_date:
+			for currency in date:
+				if currency.attrib['currency'] == src_currency:
+					src_rate = float(currency.attrib['rate'])
+				elif currency.attrib['currency'] == dest_currency:
+					dest_rate = float(currency.attrib['rate'])
+	
+	if src_currency == 'EUR':
+		src_rate = 1
+	elif dest_currency == 'EUR':
+		dest_rate = 1 
+
+	if dest_rate != 0 and src_rate != 0:
+		rate = dest_rate / src_rate
+		result = float(amount) * rate
+
 	data = {
-		"status": "ok", 
-		"message": {
-			"amount": amount,
-			"src_currency": src_currency,
-			"dest_currency": dest_currency,
-			"reference_date": reference_date
-		},
-		"data": doc.nodeName
+		"amount": result,
+		"currency": dest_currency,
 	}
+	
 	return JsonResponse(data)
